41433902a209969404014e109c5e74c02ca80514
Ubuntu <ubuntu@ip-10-252-77-82.us-west-2.compute.internal>
2012-04-03 02:51:45 +0000

    First submission commit here.

--- features/step_definitions/movie_steps.rb
+++ features/step_definitions/movie_steps.rb
@@ -1,28 +1,42 @@
 # Add a declarative step here for populating the DB with movies.
 
+movies_count = 0
 Given /the following movies exist/ do |movies_table|
   movies_table.hashes.each do |movie|
-    # each returned element will be a hash whose key is the table header.
-    # you should arrange to add that movie to the database here.
+    Movie.create!(movie)
+    movies_count += 1
   end
-  assert false, "Unimplmemented"
 end
 
 # Make sure that one string (regexp) occurs before or after another one
 #   on the same page
 
 Then /I should see "(.*)" before "(.*)"/ do |e1, e2|
-  #  ensure that that e1 occurs before e2.
-  #  page.content  is the entire content of the page as a string.
-  assert false, "Unimplmemented"
+  is_match = /#{e1}.*#{e2}/m =~ page.body
+  assert !is_match.nil?
 end
 
 # Make it easier to express checking or unchecking several boxes at once
 #  "When I uncheck the following ratings: PG, G, R"
 #  "When I check the following ratings: G"
 
-When /I (un)?check the following ratings: (.*)/ do |uncheck, rating_list|
-  # HINT: use String#split to split up the rating_list, then
-  #   iterate over the ratings and reuse the "When I check..." or
-  #   "When I uncheck..." steps in lines 89-95 of web_steps.rb
+#All movies means our count is equal to the number inserted into the db.
+Then /I should see all the movies/ do
+  page.should have_css("table#movies tbody tr", :count => movies_count.to_i)
+end
+
+#No movies means our count is zero.
+Then /I should not see any movies/ do
+  page.should have_no_css("table#movies tbody tr")
+end
+
+When /I (un)?check the following ratings: (.*)/ do |is_unchecked, rating_list|
+  rating_list.split(',').each { |r|
+    ratings_str = 'ratings[' + r.strip + ']'
+    if is_unchecked.nil?
+      uncheck(ratings_str)
+    else
+      check(ratings_str)
+    end
+  }
 end
